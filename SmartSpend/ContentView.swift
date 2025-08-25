43e4812d030566c190c2eff5eb2850ba88aa2393
RefikJaija <refikjaija3@gmail.com>
2025-08-25 13:47:53 +0200

    AddBottomSheetView separate view file

--- SmartSpend/ContentView.swift
+++ SmartSpend/ContentView.swift
@@ -45,7 +45,7 @@ struct ContentView: View {
                     AddBottomSheetView()
                         .presentationDetents([.medium, .large])
                 }
-            }else {
+            } else {
                 
                 LoginView()
             }
@@ -55,82 +55,6 @@ struct ContentView: View {
         }
     
     
-    }
-    
-    struct AddBottomSheetView: View {
-        @State private var selected = 1
-        
-        var body: some View {
-            VStack{
-                Picker("Type of add", selection: $selected){
-                    Text("Inceome").tag(1)
-                    Text("Expense").tag(2)
-                    
-                }.pickerStyle(.segmented)
-                    .padding()
-                
-                Spacer()
-                if selected == 1{
-                    
-                    Button(action: {
-                        
-                    }) {
-                        HStack {
-                            Text("Add")
-                                .foregroundStyle(.white)
-                        }
-                        .frame(maxWidth: .infinity, minHeight: 60)
-                        .background(Color.MainColor)
-                        .cornerRadius(12)
-                        .shadow(radius: 2)
-                    }
-                    .padding()
-                    
-                } else if selected == 2{
-                    VStack{
-                        
-                        
-                        Button(action: {
-                            
-                        }) {
-                            HStack {
-                                Image(systemName: "camera")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 40, height: 40)
-                                    .foregroundStyle(.gray)
-                                
-                            }
-                            .frame(maxWidth: .infinity, minHeight: 60)
-                            .background(Color.white)
-                            .cornerRadius(12)
-                            .shadow(radius: 2)
-                        }
-                        .padding()
-                        
-                        
-                        Button(action: {
-                            
-                        }) {
-                            HStack {
-                                Text("Add manually")
-                                    .foregroundStyle(.white)
-                            }
-                            .frame(maxWidth: .infinity, minHeight: 60)
-                            .background(Color.MainColor)
-                            .cornerRadius(12)
-                            .shadow(radius: 2)
-                        }
-                        .padding()
-                        
-                    }
-                }
-                
-                Spacer()
-                
-            }
-
-        }
     }
     
     
