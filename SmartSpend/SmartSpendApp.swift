6b3a7e945bc4f673b9887d46fd577181601226d6
RefikJaija <refikjaija3@gmail.com>
2025-09-19 15:42:58 +0200

    Consistent Opening

--- SmartSpend/SmartSpendApp.swift
+++ SmartSpend/SmartSpendApp.swift
@@ -22,7 +22,6 @@ struct SmartSpendApp: App {
                     ProgressView("Restoring session...") 
                 } else if authVM.isSignedIn && userVM.isOnboarded {
                     StartingScreenView(switchingView: ContentView())
-<<<<<<< HEAD
                 } else if !authVM.isSignedIn{
                     StartingScreenView(switchingView: LoginView())
                 }
@@ -30,11 +29,6 @@ struct SmartSpendApp: App {
                     OnboardView()
                 }
                 
-=======
-                } else {
-                    StartingScreenView(switchingView: LoginView())
-                }
->>>>>>> 03f5016b72bb52e06a0a244c1a4d6964325a7ce1
             }
             .environmentObject(userVM)
             .environmentObject(categoryVM)
